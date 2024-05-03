2369ef30325010b8afe0f2c012d5f9c780618136
Ned Gulley <gulley@gmail.com>
2024-05-03 16:44:53 -0400

    Update task1.m

--- Tasks/task1.m
+++ Tasks/task1.m
@@ -11,4 +11,8 @@ ttAll = readtimetable("allSolvers.csv");
 ttAll = [ttAll; tt];
 writetimetable(ttAll,"allSolvers.csv")
 
-matlab.internal.liveeditor.executeAndSave('report.mlx')
+editorObj = matlab.desktop.editor.Document.openEditorForExistingFile(which('report.mlx'), 'Visible', 0);
+fileNameToExecute = editorObj.Filename;
+executionTime = matlab.internal.liveeditor.LiveEditorUtilities.execute(editorObj.Editor.RtcId, fileNameToExecute);
+editorObj.save;
+editorObj.closeNoPrompt;
