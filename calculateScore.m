e54a1c02720409773ce860307064bcc1029114c4
Ned Gulley <gulley@gmail.com>
2024-05-06 17:20:50 -0400

    Update calculateScore.m

--- calculateScore.m
+++ calculateScore.m
@@ -1,9 +1,12 @@
 function score = calculateScore(result,computeTime)
-    % The score will ultimately be based on both the result and the
-    % computeTime. For now we'll just do a simple stand-in calculation.
+    % The score is based on both the result and the computeTime. 
 
-    k_result = 5;
-    k_computeTime = 100;
-    score = k_result*result + k_computeTime*computeTime;
+    if result > 0 
+        k_result = 5;
+        k_computeTime = 100;
+        score = k_result*result + k_computeTime*computeTime;
+    else
+        score = -1;
+    end
 
 end
