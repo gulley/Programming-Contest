108034210824ee86542d8a5eabdbba7276fc0de9
Ned Gulley <gulley@gmail.com>
2024-05-06 18:14:51 -0400

    New report

--- solver.m
+++ solver.m
@@ -1,24 +1,24 @@
 function ix = solver(x,lim)
     % Try a bunch of random permutations
 
-    ix = find(x<lim,1);
+    % ix = find(x<lim,1);
 
-    % remBest = inf;
-    % 
-    % rng(1)
-    % 
-    % for i = 1:5
-    %     rp = randperm(length(x));
-    %     xcs = cumsum(x(rp));
-    %     ix = find(xcs < lim);
-    %     ixTest = rp(ix);
-    %     rem = lim - sum(x(ixTest));
-    %     if rem < remBest
-    %         ixBest = ixTest;
-    %         remBest = rem;
-    %     end
-    % end
-    % 
-    % ix = ixBest;
+    remBest = inf;
+
+    rng(1)
+
+    for i = 1:2
+        rp = randperm(length(x));
+        xcs = cumsum(x(rp));
+        ix = find(xcs < lim);
+        ixTest = rp(ix);
+        rem = lim - sum(x(ixTest));
+        if rem < remBest
+            ixBest = ixTest;
+            remBest = rem;
+        end
+    end
+
+    ix = ixBest;
 
 end
