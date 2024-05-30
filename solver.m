ca8eef945f29d2c95f5771fe289636be918313f8
Maximilian Kuechen <maximilian.kuechen@gmail.com>
2024-05-30 10:34:36 -0400

    feat: Max's soln

--- solver.m
+++ solver.m
@@ -1,24 +1,13 @@
-function ix = solver(x,lim)
-    % Try a bunch of random permutations
-
-    % ix = find(x<lim,1);
-
-    remBest = inf;
-
-    rng(1)
-
-    for i = 1:15
-        rp = randperm(length(x));
-        xcs = cumsum(x(rp));
-        ix = find(xcs < lim);
-        ixTest = rp(ix);
-        rem = lim - sum(x(ixTest));
-        if rem < remBest
-            ixBest = ixTest;
-            remBest = rem;
+function ix = solver(x, lim)
+    bestSum = 0;
+    ix = [];
+    n = length(x);
+    for i = 1:2^n-1
+        mask = bitget(i, 1:n);
+        currentSum = sum(x(mask));
+        if currentSum > bestSum && currentSum <= lim
+            bestSum = currentSum;
+            ix = find(mask);
         end
     end
-
-    ix = ixBest;
-
 end
